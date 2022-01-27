4a81996bbd686dd67f42d866c966afd0c1f26762
doomedboi <akulenko.vv@students.dvfu.ru>
2022-01-27 21:22:45 +1000

    buf fixes + profile chart

--- fefuactivity/Profile/ProfileViewController.swift
+++ fefuactivity/Profile/ProfileViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 import Charts
 
+//  MARK:- test section
+
+
+
+var localActivities: [ActivitiesViewModel] = [ActivitiesViewModel]()
+
+
 var ddata: [[profileTableStringModel]] = {
     let profileSection: [profileTableStringModel] = [
         profileTableStringModel(type: "login:", value: ""), profileTableStringModel(type: "name:", value: ""),
@@ -27,25 +34,74 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
     @IBOutlet weak var profileSexTable: UITableView!
     @IBOutlet weak var profileDataTableView: UITableView!
     
+    var chartData: [ChartDataEntry] = []
+    
+    var localActivities: [ActivitiesViewModel] = [ActivitiesViewModel]()
+
+    
     var userM: UserModel? {
         didSet {
             setUserInfo(user: userM)
         }
     }
-    lazy var lineChartView: LineChartView = {
-        let chartView = LineChartView()
-        chartView.backgroundColor = .blue
+    
+    private func chartSetup() {
+        chartView.backgroundColor = .white
         chartView.rightAxis.enabled = false
+        chartView.xAxis.axisLineColor = .gray
+        chartView.xAxis.labelTextColor = .gray
+        chartView.xAxis.labelPosition = .bottom
         
-        return chartView
-    }()
+    
+        chartView.leftAxis.drawGridLinesEnabled = false
+        chartView.leftAxis.axisLineColor = .gray
+        chartView.leftAxis.labelTextColor = .gray
+        chartView.leftAxis.axisLineColor = .gray
+        
+        
+        chartView.leftAxis.labelCount = 2
+        
+    }
     
     func setData() {
-        let set = LineChartDataSet(entries: [], label: "")
+        let set = LineChartDataSet(entries: chartData, label: "activity distance")
+        set.drawHorizontalHighlightIndicatorEnabled = false
+        set.drawVerticalHighlightIndicatorEnabled = false
+        set.colors =  [.orange]
+        set.circleColors = [.orange]
+        set.lineWidth = 3
+        
+        set.fill =  ColorFill(cgColor: UIColor.orange.cgColor)
+        set.drawFilledEnabled = true
         
         let data = LineChartData(dataSet: set)
-        lineChartView.data = data
+        chartView.data = data
     }
+    
+    func fillChartData() {
+        fetchLocalActivities()
+        
+        //  gets activity duration from sorted activity array
+        var durationPerDate: Double = 0.0
+        var day = 1
+        for activities in localActivities {
+            for activity in activities.activities {
+                print("========")
+                print(activity.duration)
+                durationPerDate += atof(activity.duration)
+            }
+            day = Calendar.current.dateComponents([.day, .year, .month], from: activities.date).day!
+
+            chartData.append(ChartDataEntry(x: Double(day), y: durationPerDate))
+            durationPerDate = 0
+        }
+        print("=====================")
+        print(chartData)
+        //chartData.append(ChartDataEntry(x: 29.0, y: 30))
+        chartData.append(ChartDataEntry(x: 31, y: 10))
+        
+    }
+    
     //  MARK:- table view content
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 2 {
@@ -119,6 +175,7 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
                 self.profileDataTableView.reloadData()
             }
         }
+        
     }
     
     override func viewDidLoad() {
@@ -131,10 +188,50 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
         
         profileDataTableView.register(UINib(nibName: "ProfileDataTableViewCell", bundle: nil), forCellReuseIdentifier: "ProfileDataTableViewCell")
         
-        chartView.addSubview(lineChartView)
-        setData()
+        chartSetup()
         exitBtn.layer.cornerRadius = 10
         
+        fetchLocalActivities()
+        fillChartData()
+        setData()
+        
+    }
+    
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        print(entry)
     }
 }
 
+extension ProfileViewController {
+    func fetchLocalActivities() {
+        let context = FEFUCoreDataContainer.instance.context
+        
+        let fetching = CDActivity.fetchRequest()
+        
+        do {
+            let raw = try context.fetch(fetching)
+            let activityModels: [ActivitiesTableViewModel] = raw.map {
+                activity in
+                let img = UIImage(systemName: "bicycle.circle.fill") ?? UIImage()
+                return ActivitiesTableViewModel(distance: String(format: "%.2f", activity.distance), duration: String(format: "%.2f", activity.duration)+"m", activityTitle: activity.type ?? "", timeAgo: activity.date ?? Date(), icon: img, startTime: String(describing: activity.startTime!), endTime: String(describing: activity.endTime!) , nickname: "")
+            }
+            
+            let groupByDate = Dictionary(grouping: activityModels) {
+                activ in return createDateComponents(activ.timeAgo)
+            }
+            
+            self.localActivities = groupByDate.map {(key, value) in
+                return ActivitiesViewModel(date: key, activities: value)
+            }
+            
+        } catch {
+            
+        }
+    }
+    
+    private func createDateComponents(_ activityDate: Date) -> Date {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month, .day], from: activityDate)
+        return calendar.date(from: components) ?? Date()
+    }
+}
